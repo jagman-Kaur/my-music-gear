9798f9a23f77f8069deae35758bcdd6291cab74d
jagman <jagmankaur7@gmail.com>
2022-02-15 03:39:23 +1100

    added remote mysql

--- inc_dbconnect.php
+++ inc_dbconnect.php
@@ -1,11 +1,18 @@
 <?php
 $ErrorMsgs = array();
-$host = '';
-$db = '';
-$user = '';
-$charset = '';
 
-$conn = @new mysqli($host, $user, "", $db);
+//development
+// $host = 'localhost';
+// $db = 'musicgear';
+// $user = 'root';
+// $pass = '';
+
+//remote
+$host = 'sql6.freemysqlhosting.net';
+$db = 'sql6472783';
+$user = 'sql6472783';
+$pass = 'vPIJu4bWN8';
+$conn = @new mysqli($host, $user, $pass, $db);
 if ($conn -> connect_errno) //if there is no error connect_errno will be 0, oterwise >0
 	$ErrorMsgs[] = "The database server is not available. Error: " . $conn -> connect_errno . "-" . $conn -> connect_error;
 ?>
